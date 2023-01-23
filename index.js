ddc0c337705b923a4044c30a9a116f3a543bf46a
Aarav238 <aarav8090shukla@gmail.com>
2023-01-23 22:31:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/",(req,res)  => {
+    res.send(
+        "<h2>Hello bro</h2>"
+    )
+
+})
+
+
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+})
+
+
+
+//right side: port where we want to send the traffic
+//left side : traffic coming from outside world
