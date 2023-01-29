3ac534f13dc644aa2da044e63653a414a0629461
Aarav238 <aarav8090shukla@gmail.com>
2023-01-29 12:13:07 +0530

    added env variables

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const {MONGO_USER , MONGO_PASSWORD ,  MONGO_PORT , MONGO_IP} = require("./config/config.js")
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
 mongoose
-.connect("mongodb://aarav:mydatabase@mongo-db:27017/?authSource=admin")
+.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+})
 .then(() => console.log("successfully connected"))
 .catch((e) => console.log(e))
 
